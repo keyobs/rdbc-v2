e03cba1a61eb49b6a2aa30ef010e89214aa2b93d
keyobs <zeebha@gmail.com>
2026-05-13 13:09:19 +0200

    chore(config): first config with sanity and astro

---
+++ sanity/sanity.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'rdbc-v2',
+
+  projectId: 'y8v3re1a',
+  dataset: 'production',
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
