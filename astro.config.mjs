a7693c9b6fb98c7cc95368e17467a5f656730fe8
keyobs <zeebha@gmail.com>
2026-05-21 15:30:04 +0200

    config(aliases): declare folder aliases

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
+import { fileURLToPath } from "node:url";
 
-// https://astro.build/config
 export default defineConfig({
 	integrations: [react()],
+	vite: {
+		resolve: {
+			alias: {
+				"@components": fileURLToPath(new URL("./src/components", import.meta.url)),
+				"@features": fileURLToPath(new URL("./src/features", import.meta.url)),
+				"@i18n": fileURLToPath(new URL("./src/i18n", import.meta.url)),
+				"@styles": fileURLToPath(new URL("./src/styles", import.meta.url)),
+				"@layouts": fileURLToPath(new URL("./src/layouts", import.meta.url)),
+			},
+		},
+	},
 });
